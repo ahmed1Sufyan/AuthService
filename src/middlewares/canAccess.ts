8798aaa9cf370e411d37fb3f8ba9059a547f6cfb
sufyan <csc21s135@gmail.com>
2024-08-26 01:30:48 +0500

    Add middleware to check whether the admin is valid or not

---
+++ src/middlewares/canAccess.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express';
+import { Reqpayload } from '../types';
+import createHttpError from 'http-errors';
+
+export const canAccess = (permission: string[]) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        // yahan pe request ki jaga Reqpayload use krne pe routes file me jb is func ko
+        // as a middleware inject kr rhe h tw error derha h lekin Reqpayload hatakr request
+        // rehne dete h tw sahi h lekin niche ""req as Reqpayload"" use krna parega
+        const _req = req as Reqpayload;
+        const isAdmin = _req.auth.role;
+        if (!permission.includes(isAdmin)) {
+            const error = createHttpError(403, 'Permission denied');
+            return next(error);
+        }
+        next();
+    };
+};
